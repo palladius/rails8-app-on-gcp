260d9819b2c5ef1b49400033b3c9c3c8ec809070
Riccardo Carlesso <ricc+chezmoi@google.com>
2026-07-24 18:38:45 +0200

    feat: Add commenter_name to Comments with optional User association

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  belongs_to :user, optional: true
   broadcasts_to :post
+
+  def author_name
+    return user.email_address.split("@").first.capitalize if user.present?
+    return commenter_name if commenter_name.present?
+    "Anonymous"
+  end
 end
