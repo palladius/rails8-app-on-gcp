8a98c748997f75b1a075b64b97bee600db55dc72
Riccardo Carlesso <ricc+chezmoi@google.com>
2026-07-25 08:16:43 +0200

    refactor(iac): add random suffix and terraform label to cloud sql instance

--- iac/main.tf
+++ iac/main.tf
@@ -80,14 +80,21 @@ resource "random_password" "db_password" {
   special = false
 }
 
+resource "random_id" "db_suffix" {
+  byte_length = 2
+}
+
 # Cloud SQL PostgreSQL Instance
 resource "google_sql_database_instance" "main" {
-  name             = "rails8-app-on-gcp"
+  name             = "rails8-app-on-gcp-${random_id.db_suffix.hex}"
   database_version = "POSTGRES_15"
   region           = var.region
 
   settings {
     tier = "db-f1-micro"
+    user_labels = {
+      "managed-by" = "terraform"
+    }
     ip_configuration {
       ipv4_enabled = true
     }
