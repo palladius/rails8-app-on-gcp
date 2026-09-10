6d5ea91943199286bf58e88a00977dbc4fe2ab6b
Riccardo Carlesso <ricc+chezmoi@google.com>
2026-09-10 15:05:42 +0200

    test(statuses): add failing tests for STEP_8_GHI quest telemetry

--- blog/test/controllers/statuses_controller_test.rb
+++ blog/test/controllers/statuses_controller_test.rb
@@ -25,5 +25,53 @@ class StatusesControllerTest < ActionDispatch::IntegrationTest
     assert_includes json["storage"], "blobs_count"
     assert json["ai"].present?
     assert json["jobs"].present?
+    assert json["quest"].present?
+  end
+
+  test "should return quest status with step_8_completed false when STEP_8_GHI is not set" do
+    old_val = ENV["STEP_8_GHI"]
+    ENV.delete("STEP_8_GHI")
+    begin
+      get status_url(format: :json)
+      assert_response :success
+      json = JSON.parse(response.body)
+      assert_equal false, json.dig("quest", "step_8_completed")
+      assert_nil json.dig("quest", "ghi_issue")
+      assert_nil json.dig("quest", "ghi_url")
+    ensure
+      ENV["STEP_8_GHI"] = old_val if old_val
+    end
+  end
+
+  test "should parse STEP_8_GHI numeric string and return canonical issue URL" do
+    old_val = ENV["STEP_8_GHI"]
+    ENV["STEP_8_GHI"] = "83"
+    begin
+      get status_url(format: :json)
+      assert_response :success
+      json = JSON.parse(response.body)
+      assert_equal true, json.dig("quest", "step_8_completed")
+      assert_equal 83, json.dig("quest", "ghi_issue")
+      assert_equal "https://github.com/palladius/rails8-app-on-gcp/issues/83", json.dig("quest", "ghi_url")
+    ensure
+      ENV.delete("STEP_8_GHI")
+      ENV["STEP_8_GHI"] = old_val if old_val
+    end
+  end
+
+  test "should parse STEP_8_GHI full URL and return canonical issue URL and number" do
+    old_val = ENV["STEP_8_GHI"]
+    ENV["STEP_8_GHI"] = "https://github.com/palladius/rails8-app-on-gcp/issues/83"
+    begin
+      get status_url(format: :json)
+      assert_response :success
+      json = JSON.parse(response.body)
+      assert_equal true, json.dig("quest", "step_8_completed")
+      assert_equal 83, json.dig("quest", "ghi_issue")
+      assert_equal "https://github.com/palladius/rails8-app-on-gcp/issues/83", json.dig("quest", "ghi_url")
+    ensure
+      ENV.delete("STEP_8_GHI")
+      ENV["STEP_8_GHI"] = old_val if old_val
+    end
   end
 end
